eb41da1efce7d2a27bcde0cc92a108e5534ab62d
Minato <umi0n3@gmail.com>
2024-12-29 15:42:21 +0100

    Tests checkpoint

---
+++ tests/Booking.Core.Tests/FakeData/FakeBookingRequestValidData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using Booking.Core.Enums;
+using Booking.Core.Models;
+
+namespace Booking.Core.Tests.FakeData;
+
+public class FakeBookingRequestValidData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return [GetValid()];
+        yield return [GetValidFromDateOnly()];
+        yield return [GetValidToDateOnly()];
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    
+    private static BookingRequest GetValid()
+    {
+        var request = new BookingRequest(
+            "H1",
+            new DateOnly(2024, 9, 1), 
+            new DateOnly(2024, 9, 3), 
+            RoomTypeCode.DBL);
+        
+        return request; 
+    }
+
+    private static BookingRequest GetValidFromDateOnly()
+    {
+        var request = new BookingRequest(
+            "H1",
+            new DateOnly(2024, 9, 1), 
+            null, 
+            RoomTypeCode.DBL);
+        
+        return request;
+    }
+    
+    private static BookingRequest GetValidToDateOnly()
+    {
+        var request = new BookingRequest(
+            "H1",
+            null, 
+            new DateOnly(2024, 9, 3), 
+            RoomTypeCode.DBL);
+        
+        return request; 
+    }
+}
--- tests/Booking.Core.Tests/Services/DataServiceTests.cs
+++ tests/Booking.Core.Tests/Services/DataServiceTests.cs
@@ -1,8 +1,8 @@
 ﻿using System.Text.Json;
 using Booking.Core.Abstracts;
-using Booking.Core.Enums;
 using Booking.Core.Models;
 using Booking.Core.Services;
+using Booking.Core.Tests.FakeData;
 using FluentAssertions;
 using Moq;
 
@@ -10,61 +10,18 @@ namespace Booking.Core.Tests.Services;
 
 public class DataServiceTests
 {
+    private readonly Mock<IStream> _mockFileStream = new();
+
     [Fact]
     public void GetDataTestGenericHotelClassShouldNotBeNullOrEmpty()
     {
         //Arrange
-        
-        //lang=JSON
-        const string jsonHotels = """
-                                  [
-                                    {
-                                      "id": "H1",
-                                      "name": "Hotel California",
-                                      "roomTypes": [
-                                        {
-                                          "code": "SGL",
-                                          "description": "Single Room",
-                                          "amenities": ["WiFi", "TV"],
-                                          "features": ["Non-smoking"]
-                                        },
-                                        {
-                                          "code": "DBL",
-                                          "description": "Double Room",
-                                          "amenities": ["WiFi", "TV", "Minibar"],
-                                          "features": ["Non-smoking", "Sea View"]
-                                        }
-                                      ],
-                                      "rooms": [
-                                        {
-                                          "roomType": "SGL",
-                                          "roomId": "101"
-                                        },
-                                        {
-                                          "roomType": "SGL",
-                                          "roomId": "102"
-                                        },
-                                        {
-                                          "roomType": "DBL",
-                                          "roomId": "201"
-                                        },
-                                        {
-                                          "roomType": "DBL",
-                                          "roomId": "202"
-                                        }
-                                      ]
-                                    }
-                                  ] 
-                                  """;
-
-        var mockFileStream = new Mock<IStream>();
-        
-        mockFileStream
+        _mockFileStream
             .Setup(x => x.GetStream(It.IsAny<string>(), FileMode.Open, FileAccess.Read, FileShare.Read))
-            .Returns(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(jsonHotels)));
+            .Returns(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(DataHelper.JsonHotels)));
 
         //Act
-        var dataService = new DataService(mockFileStream.Object);
+        var dataService = new DataService(_mockFileStream.Object);
         var result = dataService.GetData<Hotel>(It.IsAny<string>());
         
         //Assert
@@ -75,12 +32,105 @@ public class DataServiceTests
     }
 
     [Fact]
-    public void GetDataTestBookingClassShouldNotBeNullOrEmpty()
+    public void GetDataTestGenericBookingClassShouldNotBeNullOrEmpty()
+    {
+        //Arrange
+        _mockFileStream
+            .Setup(x => x.GetStream(It.IsAny<string>(), FileMode.Open, FileAccess.Read, FileShare.Read))
+            .Returns(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(DataHelper.JsonBookings)));
+        
+        //Act
+        var dataService = new DataService(_mockFileStream.Object);
+        var result = dataService.GetData<Models.Booking>(It.IsAny<string>());
+        
+        //Assert
+        result
+            .Should().NotBeNullOrEmpty()
+            .And.HaveCount(2)
+            .And.ContainItemsAssignableTo<Models.Booking>();
+    }
+
+    [Theory]
+    [ClassData(typeof(FakeJsonInvalidData))]
+    public void GetDataTestGenericHotelClassShouldThrowJsonException(string json)
+    {
+        //Arrange
+        _mockFileStream
+            .Setup(x => x.GetStream(It.IsAny<string>(), FileMode.Open, FileAccess.Read, FileShare.Read))
+            .Returns(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)));
+        
+        //Act
+        var dataService = new DataService(_mockFileStream.Object);
+        var func = () => dataService.GetData<Hotel>(It.IsAny<string>());
+        
+        //Assert
+        func.Should().Throw<JsonException>();
+    }
+
+    [Theory]
+    [ClassData(typeof(FakeJsonInvalidData))]
+    public void GetDataTestGenericBookingClassShouldThrowJsonException(string json)
+    {
+        //Arrange
+        _mockFileStream
+            .Setup(x => x.GetStream(It.IsAny<string>(), FileMode.Open, FileAccess.Read, FileShare.Read))
+            .Returns(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)));
+        
+        //Act
+        var dataService = new DataService(_mockFileStream.Object);
+        var func = () => dataService.GetData<Models.Booking>(It.IsAny<string>());
+        
+        //Assert
+        func.Should().Throw<JsonException>();
+    }
+
+    [Theory]
+    [ClassData(typeof(FakeBookingRequestValidData))]
+    public void GetResponseTestShouldNotBeNull(BookingRequest request)
+    {
+        //Arrange
+        var expected = DataHelper.GetHotelResponses().ToList();
+        
+        //Act
+        var dataService = new DataService(_mockFileStream.Object);
+        
+        var actual = dataService.GetResponse(
+            request,
+            DataHelper.GetValidHotels(), 
+            DataHelper.GetValidBookings());
+        
+        var responses = actual.ToList();
+        var rooms = responses[0].Rooms;
+
+        //Assert
+        responses
+            .Should().NotBeNull()
+            .And.HaveCount(1)
+            .And.ContainItemsAssignableTo<HotelResponse>()
+            .And.BeEquivalentTo(expected);
+
+        rooms
+            .Should().NotBeNull()
+            .And.HaveCount(2)
+            .And.ContainItemsAssignableTo<Room>()
+            .And.BeEquivalentTo(expected[0].Rooms);
+    }
+
+    [Theory]
+    [ClassData(typeof(FakeBookingRequestInvalidData))]
+    public void GetResponseTestShouldBeNull(BookingRequest request)
     {
         //Arrange
         
         //Act
+        var dataService = new DataService(_mockFileStream.Object);
         
-        //Arrange
+        var actual = dataService.GetResponse(
+            request,
+            DataHelper.GetValidHotels(),
+            DataHelper.GetValidBookings());
+        
+        //Assert
+        actual.Should().BeEmpty();
     }
 }
